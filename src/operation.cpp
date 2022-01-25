37fc54b13d88bee5eb0ce7301c0ed5a8a861fc55
HetDaftary <hetdaftary@gmail.com>
2022-01-25 11:06:37 +0530

    Improving code of join funciton

--- src/operation.cpp
+++ src/operation.cpp
@@ -1,6 +1,6 @@
 #include "../include/operation.hpp"
 
-
+#include <cstring>
 #include "../include/logger.hpp"
 #include "../include/products.hpp"
 
@@ -8,17 +8,35 @@ static string seperator = ",";
 string ordersFileName = "data/orders.txt";
 mutex ordersFileMutex;
 
+/**
+ * @brief Joins n strings with Delim.
+ * 
+ * @param delim 
+ * @param strings 
+ * @return string 
+ */
 string join(string delim, vector<string> strings) {
-    string result = "";
-    for (int i = 0; i < strings.size() - 1; i++) {
-        //std::cout << strings[i] << std::endl;
-        result = result + strings[i];
-        result = result + delim;
-    }
-    result = result + strings[strings.size() - 1];
-    //std::cout << result;
+    int size = strings.size() - 1;
+    int sizeToReturn = size * delim.size();
 
-    return result;
+    int toStartAt[size + 1];
+    toStartAt[0] = 0;
+
+    for (int i = 0; i < size; i++) {
+        sizeToReturn += strings[i].size();
+        toStartAt[i + 1] = toStartAt[i] + strings[i].size() + delim.size();
+    } sizeToReturn += strings[size].size();
+
+    char* result = new char[sizeToReturn + 1];
+
+    for (int i = 0; i < size; i++) {
+        strcpy(result + toStartAt[i], strings[i].c_str());
+        strcpy(result + toStartAt[i] + strings[i].size(), delim.c_str());  
+    }
+
+    strcpy(result + toStartAt[size], strings[size].c_str());
+
+    return string(result);
 }
 
 typedef struct {
